8ec4865048b45b886d25edbe47083fecd90601a9
Jeremy Rand <jeremy@rand-family.com>
2026-07-15 14:13:00 -0400

    Add a SCSI command abstraction

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-#include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
 #include <string.h>
@@ -8,24 +7,16 @@
 #include <Directory.h>
 #include <Entry.h>
 #include <Path.h>
-#include <scsi.h>
-#include <CAM.h>
+
+#include "SCSICommand.h"
 
 
 #define SCSI_BUS "/dev/bus/scsi"
 #define SCSI_RAW_DEV_NAME "raw"
-#define SCSI_SENSE_SIZE 16
-#define SCSI_TIMEOUT 1000000
-
-#define SCSI_INQUIRY 0x12
 
 static void inquiry(const char * dev)
 {
 	int fd;
-	int e;
-	raw_device_command rdc;
-	scsi_inquiry data;
-	uchar sense[SCSI_SENSE_SIZE];
 	
 	printf("Running inquiry against dev = %s\n", dev);
 	if ((fd = open(dev, 0)) < 0) {
@@ -33,34 +24,27 @@ static void inquiry(const char * dev)
 		return;
 	}
 	
-	rdc.data = &data;
-	rdc.data_length = sizeof(data);
-	rdc.sense_data = sense;
-	rdc.sense_data_length = 0;
-	rdc.timeout = SCSI_TIMEOUT;
-	rdc.flags = B_RAW_DEVICE_DATA_IN;
-	rdc.command_length = 6;
-	rdc.command[0] = SCSI_INQUIRY;
-	rdc.command[1] = 0x00;
-	rdc.command[2] = 0x00;
-	rdc.command[3] = 0x00;
-	rdc.command[4] = sizeof(data);
-	rdc.command[5] = 0x00;
+	SCSIInquiryResult inqResult;
+	SCSICommand comm(fd);
 	
-	e = ioctl(fd, B_RAW_DEVICE_COMMAND, &rdc, sizeof(rdc));
-	if (e != 0) {
-		fprintf(stderr, "Error from ioctl of dev %s, %s\n", dev, strerror(errno));
-		close(fd);
+	if (!comm.Inquiry(&inqResult)) {
+		fprintf(stderr, "ERROR: %s\n", comm.GetErrorStr());
+		return;
 	}
 	
-	if (rdc.cam_status != CAM_REQ_CMP) {
-		fprintf(stderr, "Unexpected cam status on dev %s, %x\n", dev, (int) rdc.cam_status);
-	}
+	printf("  Type:    %u (%s)\n", (uint32)inqResult.type, inqResult.typeStr);
+	printf("  Vendor:  \"%s\"\n", inqResult.vendor);
+	printf("  Device:  \"%s\"\n", inqResult.device);
+	printf("  Version: \"%s\"\n", inqResult.version);
 	
-	for (int i = 0; i < sizeof(data.inquiry_data); i++) {
-		uchar ch = data.inquiry_data[i];
-		printf("%2x: %2x %c\n", i, (int)ch, (isprint(ch) ? ch : ' '));
+	const uint8 * sense = comm.GetSense();
+	printf("  Sense:");
+	for (int i = 0; i < SCSI_SENSE_SIZE; i++) {
+		if ((i % 8) == 0)
+			printf("\n      ");
+		printf("0x%02x ", (int)sense[i]);
 	}
+	printf("\n\n");
 	
 	close(fd);
 }
@@ -81,7 +65,7 @@ static void walkDevs(const char * path)
 	}
 }
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[]) 
 {
 	if (argc >= 2)
 		inquiry(argv[1]);
